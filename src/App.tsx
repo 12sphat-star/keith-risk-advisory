f6e4789ebf8bbbb57220aa0483e74c07c936f729
Keith Robertson <keithrobertson@Keiths-MacBook-Pro.local>
2026-03-10 23:40:08 -0400

    Add mobile-only css import

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
+import "./styles/mobile-only.css"
 
 import Home from "./pages/Home"
 import HealthInsurance from "./pages/HealthInsurance"
